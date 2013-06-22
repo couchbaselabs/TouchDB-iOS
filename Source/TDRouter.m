85d27611e08516e318e368ced3c3406c86a28f28
Matias Piipari <matias.piipari@gmail.com>
2013-06-22 15:43:43 +0100

    Fixed the crashing bug https://github.com/couchbaselabs/TouchDB-iOS/issues/266

--- Source/TDRouter.m
+++ Source/TDRouter.m
@@ -586,11 +586,13 @@ static NSArray* splitPath( NSURL* url ) {
 
 
 - (void) stopNow {
-    _running = NO;
-    self.onResponseReady = nil;
-    self.onDataAvailable = nil;
-    self.onFinished = nil;
-    [[NSNotificationCenter defaultCenter] removeObserver: self];
+    @synchronized(self) {
+        _running = NO;
+        self.onResponseReady = nil;
+        self.onDataAvailable = nil;
+        self.onFinished = nil;
+        [[NSNotificationCenter defaultCenter] removeObserver: self];
+    }
 }
 
 
