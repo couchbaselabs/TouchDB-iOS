7b2999943031322d2055a4feb2b3b0182b3f1b2d
Matias Piipari <matias.piipari@gmail.com>
2013-06-24 20:32:27 +0100

    Revert "Fixed the crashing bug https://github.com/couchbaselabs/TouchDB-iOS/issues/266"
    This reverts commit 85d27611e08516e318e368ced3c3406c86a28f28.

--- Source/TDRouter.m
+++ Source/TDRouter.m
@@ -586,13 +586,11 @@ static NSArray* splitPath( NSURL* url ) {
 
 
 - (void) stopNow {
-    @synchronized(self) {
-        _running = NO;
-        self.onResponseReady = nil;
-        self.onDataAvailable = nil;
-        self.onFinished = nil;
-        [[NSNotificationCenter defaultCenter] removeObserver: self];
-    }
+    _running = NO;
+    self.onResponseReady = nil;
+    self.onDataAvailable = nil;
+    self.onFinished = nil;
+    [[NSNotificationCenter defaultCenter] removeObserver: self];
 }
 
 
